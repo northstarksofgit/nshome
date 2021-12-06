b6877eb96138d2bbcb5c720468ba6d472f2c60df
hyeonmin.cho <hyeonmin.cho@northstar.co.kr>
2021-12-06 16:34:39 +0900

    clientLIbs / kia30-site -> kia25-site 로 수정

--- kia25/core/src/main/java/com/kia25/core/models/Build004Helper.java
+++ kia25/core/src/main/java/com/kia25/core/models/Build004Helper.java
@@ -1,13 +1,10 @@
 package com.kia25.core.models;
 
-import java.io.IOException;
-
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
@@ -15,32 +12,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.kia25.core.rest.client.dto.CategoryListDto;
+import com.kia25.core.rest.client.service.Build004Service;
 import com.kia25.core.rest.client.service.BuildYourCarService;
+import com.kia25.core.rest.client.service.impl.Build004ServiceImpl;
 
 
 @Model(adaptables = { SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class Build004Helper {
 
-	private final static Logger log = LoggerFactory.getLogger(Build004Helper.class);
+private final static Logger log = LoggerFactory.getLogger(Build004Helper.class);
 	
 	@OSGiService
-	BuildYourCarService service;
+	Build004Service service= new Build004ServiceImpl();
+//	BuildYourCarService service;	
 
-	/**
-	 * 카테고리 리스트
-	 */
+	@Inject
+	private Resource resource;
+
 	private CategoryListDto categoryList;
-	
-	
-    @PostConstruct
-	public void activate() throws IOException {
-    	categoryList = service.getCategoryList();
-    	log.info("categoryList ::: {}", categoryList);
-    }
 
-
+	@PostConstruct
+	public void activate() throws Exception {
+		categoryList = service.getCategoryList();
+	
+	}
+	
 	public CategoryListDto getCategoryList() {
 		return categoryList;
 	}
-	
+
 }
---
+++ kia25/core/src/main/java/com/kia25/core/rest/client/service/Build004Service.java
@@ -0,0 +1,12 @@
+package com.kia25.core.rest.client.service;
+
+import com.kia25.core.rest.client.dto.CategoryListDto;
+
+public interface Build004Service {
+
+	//build001컴포넌트에서 사용하는 Service
+	//필요시 추후 다른 Service로 통합 예정
+	
+	CategoryListDto getCategoryList();
+	
+}

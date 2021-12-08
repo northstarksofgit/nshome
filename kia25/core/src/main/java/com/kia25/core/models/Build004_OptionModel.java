25788de48d05e5148486d6807462214058e117f5
hyeonmin.cho <hyeonmin.cho@northstar.co.kr>
2021-12-08 16:39:26 +0900

    Build004Helper.java 파일명 변경 -> Build004_OptionModel.java

--- kia25/core/src/main/java/com/kia25/core/models/Build004_OptionModel.java
+++ kia25/core/src/main/java/com/kia25/core/models/Build004_OptionModel.java
@@ -9,41 +9,39 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.kia25.core.rest.client.dto.CategoryListDto;
-import com.kia25.core.rest.client.dto.ModelDto;
-import com.kia25.core.rest.client.service.Build004Service;
+import com.kia25.core.rest.client.dto.option.OptionDto;
 import com.kia25.core.rest.client.service.BuildYourCarService;
-import com.kia25.core.rest.client.service.impl.Build004ServiceImpl;
 import com.kia25.core.rest.client.service.impl.BuildYourCarServiceImpl;
 
 
 @Model(adaptables = { SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class Build004Helper {
+public class Build004_OptionModel {
 
-private final static Logger log = LoggerFactory.getLogger(Build004Helper.class);
+private final static Logger log = LoggerFactory.getLogger(Build004_OptionModel.class);
 	
 	BuildYourCarService buildYourCarService = new BuildYourCarServiceImpl();
 	@Inject
 	private Resource resource;
 
-	private List<ModelDto> modelList;
+	private List<OptionDto> optionList;
 
 	@PostConstruct
 	public void activate() throws Exception {
-		modelList = buildYourCarService.getModelListAPI().getModelList();
-		log.info("getModelList :::: {}", modelList);
-	}
-
-	public List<ModelDto> getModelList() {
-		return modelList;
+		optionList = buildYourCarService.getOptionlListAPI().getListOfOptions();
+		log.info("getOptionList :::: {}", optionList);
 	}
 
 
-	public void setModelList(List<ModelDto> modelList) {
-		this.modelList = modelList;
+	public List<OptionDto> getOptionList() {
+		return optionList;
 	}
+
+	public void setOptionList(List<OptionDto> optionList) {
+		this.optionList = optionList;
+	}
+
+
 }

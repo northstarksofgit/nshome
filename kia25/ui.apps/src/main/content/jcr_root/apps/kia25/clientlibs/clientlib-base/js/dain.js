a4e041733fd23636848550ab657a69074c4befda
dada411 <ohda0411@gmail.com>
2021-12-10 13:45:34 +0900

    json server 파일 업로드 / build002 html 및 js 수정중

--- kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dain.js
+++ kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dain.js
@@ -1,42 +1,39 @@
 $(document).ready(function() {
-	
-	$('.form_spr radio_box').click(function() {
-		
-		$(this).hasClass("on") ? $(this).removeClass("on") : $(this).addClass("on");
-		
-		
-		
-	})
-	
-	
-	
-	
-	/*
+
 	var selectedTrimList = [];
 	var selectedTrimPrice = 0;
 	
-    $('.form_spr radio_box').click(function(e) {
+	
+    $('.form_chk').click(function() {
 
+		console.log($(this).children('input').val());
+		console.log($('.form_chk'));
+		
+		if($(this).attr('checked') == true && $(this).attr('checked') != true) {
+			
+			$(this).children('input').prop('checked', false);
+			$(this).children('input').attr('checked', false);
+			
+		} else {
+			
+			$(this).children('input').prop('checked', true);
+			$(this).children('input').attr('checked', true);
+		}
+		
+	})
+  
 
-        $(this).hasClass("on") ? $(this).removeClass("on") : $(this).addClass("on")
+	 $('.box_u').click(function() {
+        
 
-
-        if(selectedTrimList.indexOf($(this)[0].dataset.opt) > -1) {
-			var idx = selectedTrimList.indexOf($(this)[0].dataset.tirm);
-            delete selectedTrimList[idx];
-
-			selectedTrimPrice = parseInt(selectedTrimPrice) - parseInt($(this)[0].dataset.price);
-			selectedTrimList = selectedTrimList.filter(Boolean) 
-
-        } else {
-        	selectedTrimList.push($(this)[0].dataset.opt);
-			selectedTrimPrice = parseInt(selectedTrimPrice) + parseInt($(this)[0].dataset.price);
-        }
-
-
-        $('.info_area.option_info').find('.info_text').children()[0].textContent = selectedTrimList.length;
-		$('.info_area.option_info').find('.box_info').children().find('.num')[0].textContent = selectedTrimPrice.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+         console.log($(this).children('.trimselector').children('.trim_click.on').attr('value'));
+		
+		
+		
+         $('.box_u').find('a').children().find('.price')[0].textContent = price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 
     })
-*/
+
+
+
 })

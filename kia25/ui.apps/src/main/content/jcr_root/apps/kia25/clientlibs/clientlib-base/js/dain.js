bde0c3fa92b0ee5aa5996721a338b7ede2b0f9c5
dada411 <ohda0411@gmail.com>
2021-12-15 13:44:24 +0900

    build002 trimCode 관련 js 수정중

--- kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dain.js
+++ kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dain.js
@@ -1,13 +1,19 @@
+
+	// navi
+	var naviTrimCode = null;
+	
 $(document).ready(function() {
 	
 	var selectedTrimCode = null;
-	var radioIntVal = $("input:radio[name=sample1]");
+	var radioInitVal = $("input:radio[name=sample1]");
 	
-	for(var i=0; i< radioIntVal.length; i++){
+	loadTrim();
+	
+	for(var i=0; i< radioInitVal.length; i++){
 		
-		if(radioIntVal[i].checked){
+		if(radioInitVal[i].checked){
 			
-			var radioId = radioIntVal[i].id;
+			var radioId = radioInitVal[i].id;
 			selectedTrimCode = $('#'+radioId).parent()[0].dataset.trimcode;		
 			
 			showHideEvent(selectedTrimCode);
@@ -22,22 +28,23 @@ $(document).ready(function() {
 		for(var i = 0; i < trimlist.length; i++){
 			
 			if(trimlist[i].dataset.car != _trimCode){
+				
 				trimlist[i].style.display = 'none';
+				
 			} else {
+				
 				trimlist[i].style.display = 'block';
 				
 				var classAdd = $(trimlist[i]).find('li')[0];
 				
 				if ($(trimlist[i]).find('li')[0].dataset.seq == 1){
-					
 					$(classAdd).addClass('on');
+					
 				}
 			}
 		}
 	}
 	
-	
-	$('.form_chk.carGroup').trigger('click');
 	
     $('.form_chk.carGroup').click(function() {
 	
@@ -46,14 +53,35 @@ $(document).ready(function() {
 		
 		showHideEvent(selectedTrimCode);
 		
+		loadTrim();
+		
 	})
+	
+	function loadTrim(){
+		
+		var aa = $('.trim_click')
+		console.log(aa);
+		
+		for(var i = 0; i < aa.length; i++){
+			
+			if($(aa[i]).hasClass('on')){
+				naviTrimCode = aa[i].dataset.trim;
+				console.log(naviTrimCode);
+			}
+		}
+	}
+	
 	
 	$('.trim_click').on('click', function(e){
 		e.preventDefault();
 		
 		$('.trim_click').removeClass('on');
 		$(this).addClass('on');
+		
+		naviTrimCode = $(this).find('#naviTrimCode').val();
+		
 	})
+	
 
 
 })

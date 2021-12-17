38b4cd884bd950c9d764c6885f16dfbd36a5ea9f
dada411 <ohda0411@gmail.com>
2021-12-17 11:32:07 +0900

    build002 관련 service / serviceImpl 수정

--- kia25/core/src/main/java/com/kia25/core/models/Build002Model.java
+++ kia25/core/src/main/java/com/kia25/core/models/Build002Model.java
@@ -15,6 +15,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.kia25.core.rest.client.dto.CarGroupDto;
+import com.kia25.core.rest.client.dto.ModelDetailListDto;
+import com.kia25.core.rest.client.dto.ModelDetailListDtoResults;
+import com.kia25.core.rest.client.dto.TransmissionDto;
 import com.kia25.core.rest.client.dto.TrimDto;
 import com.kia25.core.rest.client.service.BuildYourCarService;
 import com.kia25.core.rest.client.service.impl.BuildYourCarServiceImpl;
@@ -35,55 +38,43 @@ public class Build002Model {
 	//private String modelCode = service.getCarGroupListAPI().getData().getModelCode();
 	
 	
-	/**
-	 * carGroup 리스트
-	 */
+	private ModelDetailListDtoResults detailResults;
+	
+	private ModelDetailListDto detailData;
+	
 	private List<CarGroupDto> carGroupList;
 
+	private List<TransmissionDto> transmissionList;
 	
-	/**
-	 * trim 리스트
-	 */
 	private List<TrimDto> trimList;
-	
-	
-	/**
-	 * transmission 리스트
-	 */
-	private List<CarGroupDto> transList;
-	
-	
-	/**
-	 * transmission trim 리스트
-	 */
-	private List<TrimDto> transTrimList;
-	
 	
 	private List<Map<String, Object>> test = new ArrayList<Map<String, Object>>();
 	
 	 
-//	@PostConstruct
-//	public void activate() throws Exception{
-//		
-//		/**
-//		 * 파라미터에서 modelCode 받아와서 변수 선언
-//		 */
-//		reqParam = request.getParameter("modelCode");
-//
-//		// 차종이 EV6인 경우 
-//		if(reqParam.equals("EV6")) {
-//			
-//			carResults = service.getCarGroupListAPI();
-//			
-//			carData = carResults.getData();
-//			carGroupList = carData.getCarGroupList();
-//			
-//			int size = carGroupList.size();
-//				
-//			for(CarGroupDto aa : carGroupList){
-//				int seq = 1; 
-//				
-//				for(TrimDto bb : aa.getTrimList()) {
+	@PostConstruct
+	public void activate() throws Exception{
+		
+		/**
+		 * 파라미터에서 modelCode 받아와서 변수 선언
+		 */
+		reqParam = request.getParameter("modelCode");
+
+		// 차종이 EV6인 경우 
+		if(reqParam.equals("EV6")) {
+			
+			detailResults = service.getModelDetailEAPI();
+			detailData = detailResults.getData();
+			carGroupList = detailData.getCarGroupList();
+			transmissionList = detailData.getTransmissionList();
+			trimList = detailData.getTrimList();
+			
+			
+			int size = carGroupList.size();
+				
+			for(CarGroupDto aa : carGroupList){
+				int seq = 1; 
+				
+//				for(TrimDto bb : aa.getCarGroupCode()) {
 //					
 //					Map<String, Object> map = new HashMap<String, Object>();
 //					map.put("carGroupCode", aa.getCarGroupCode());
@@ -96,22 +87,22 @@ public class Build002Model {
 //					map.put("compoundFuelEconomy", bb.getCompoundFuelEconomy());
 //					map.put("bodyTypeName", bb.getBodyTypeName());
 //					map.put("engineCapacityName", bb.getEngineCapacityName());
-//					map.put("transmission", bb.getTransmission());
 //					map.put("seq", seq++);
 //					test.add(map);
 //				}
-//			}
-//			
-//		// 차종이 EV6가 아닌 경우 
-//		} else {
-//			
-//			carResults = service.getTrasmissionListAPI();
-//			
-//			carData = carResults.getData();
-//			carGroupList = carData.getCarGroupList();
-//			
-//			int size = carGroupList.size();
-//			
+			}
+			
+		// 차종이 EV6가 아닌 경우 (성력)
+		} else {
+			
+			detailResults = service.getModelDetailSAPI();
+			detailData = detailResults.getData();
+			carGroupList = detailData.getCarGroupList();
+			transmissionList = detailData.getTransmissionList();
+			trimList = detailData.getTrimList();
+			
+			int size = carGroupList.size();
+			
 //			for(CarGroupDto trans : carGroupList){
 //				int auto_seq = 1; 
 //				int manu_seq = 1; 
@@ -129,9 +120,9 @@ public class Build002Model {
 //					map.put("compoundFuelEconomy", transTrim.getCompoundFuelEconomy());
 //					map.put("bodyTypeName", transTrim.getBodyTypeName());
 //					map.put("engineCapacityName", transTrim.getEngineCapacityName());
-//					map.put("transmission", transTrim.getTransmission());
 //					
-//					if(transTrim.getTransmission().equals("automatic")) {
+//					
+//					if(transTrim.getTransmissionCode().equals("automatic")) {
 //						
 //						map.put("seq", auto_seq++);
 //						
@@ -143,9 +134,10 @@ public class Build002Model {
 //					
 //					test.add(map);
 //				}
-//			}
-//		}
-//	}
+			
+			
+		}
+	}
 	
 	
 	/**

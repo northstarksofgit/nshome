bbcb6fef21d8b620cb79231e2f1a3f784cf563d2
hyeonmin.cho <hyeonmin.cho@northstar.co.kr>
2021-12-28 18:06:47 +0900

    build004-crud 중간커밋

--- kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/hm-crud.js
+++ kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/hm-crud.js
@@ -1,21 +1,87 @@
 var optionListURL = "http://192.168.31.86:9090/build-your-car//option-list?trimCode=EV-T001&modelCode=EV6&ext=null&int=null";
+//Get the modal
 
 $(function() {
-	$.ajax({
-		type:"GET",
-		url : optionListURL,
-		success:function( res){
-			console.log(res);
-		}
+//	$.ajax({
+//		type:"GET",
+//		url : optionListURL,
+//		success:function( res){
+//			console.log(res);
+//		}
+//	})
+	
+	
+//	var modal = document.getElementById("myModal");
+//
+//	// Get the button that opens the modal
+//	var btn = document.getElementById("myBtn");
+//
+//	// Get the <span> element that closes the modal
+//	var span = document.getElementsByClassName("close")[0];
+
+//	btn.onclick = function() {
+//	  modal.style.display = "block";
+//	}
+//
+//	// When the user clicks on <span> (x), close the modal
+//	span.onclick = function() {
+//	  modal.style.display = "none";
+//	}
+//
+//	// When the user clicks anywhere outside of the modal, close it
+//	window.onclick = function(event) {
+//	  if (event.target == modal) {
+//	    modal.style.display = "none";
+//	  }
+//	}
+		
+	
+	var dataObj;
+	$('.btn-edit').click(function() {
+		var tdList = $(this).parents('tr').children();
+		var mode = 'E';
+		dataObj = returnData(tdList , mode);
+		
+		$('.modal').css('display', 'block')
+	})
+	
+	
+	$('.btn-delete').click(function() {
+		var tdList = $(this).parents('tr').children();
+		var mode = 'D'
+		dataObj = returnData(tdList , mode);
+	})
+	
+	$('.close').click(function() {
+		dataObj = null;
+		$('.modal').css('display', 'none');
 	})
 })
 
 
+function returnData(data, mode) {
+	var tdList = data;
+	var dataObj = {};
+	
+	tdList.each(function(i){
+		if(tdList.eq(i)[0].dataset.field) {
+			if(mode == 'E') {
+				if(tdList.eq(i)[0].dataset.value == 'null') {
+					dataObj[tdList.eq(i)[0].dataset.field] = null; 
+				} else {
+					dataObj[tdList.eq(i)[0].dataset.field] = tdList.eq(i)[0].dataset.value;
+				}
+			} else {
+				if(tdList.eq(i)[0].dataset.field == 'optionCode') {
+					dataObj[tdList.eq(i)[0].dataset.field] = tdList.eq(i)[0].dataset.value;
+				}
+			}
+		}
+	});
 
-$('.btn-edit').on('click', function() {
-	console.log($(this).parent().parent().parent())
-})
-
+	return dataObj;
+	
+}
 
 //테이블의 Row 클릭시 값 가져오기
 //$("#example-table-1 tr").click(function(){

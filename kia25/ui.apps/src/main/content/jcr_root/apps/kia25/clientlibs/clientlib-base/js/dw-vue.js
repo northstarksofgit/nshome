bc1c53102bbb13d3a8a758676d580017d167c1f1
username-bb <jdw0477@gmail.com>
2022-01-10 17:00:45 +0900

    category css 관련 js 수정

--- kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dw-vue.js
+++ kia25/ui.apps/src/main/content/jcr_root/apps/kia25/clientlibs/clientlib-base/js/dw-vue.js
@@ -4,35 +4,53 @@
 
 $(()=>{
 
-	if(document.getElementById('vue-app') != null ){	
-	
+	//build-001.html
+	if(document.getElementById('vue-app-001') != null ){
+			
 		new Vue({
 			
-			el: '#vue-app',
-	
+			el: '#vue-app-001',
 			created: function(){
 				
 				const vm = this;
 				window.addEventListener('scroll', vm.cateogryScroll);
 
-			}, //created 닫기
+			}, 
 			
 			methods:{
-				
 				cateogryScroll,
-				
-				
-				
-			} //method 닫기
+			} 
 			
-			
-			
-			
-			
-			
-		})// vue 인스턴스 닫기
+		});// vue 인스턴스 닫기
 	
-	}//vue-app null check 닫기
+	};//vue-app-001 check 닫기
+	
+	
+	//navi.html
+	if(document.getElementById('vue-app-navi') != null ){
+			
+		new Vue({
+			
+			el: '#vue-app-navi',
+			created: function(){
+				
+				const vm = this;
+
+			}, 
+			
+			methods:{
+			} 
+			
+		});// vue 인스턴스 닫기
+	
+	};//vue-app-001 check 닫기
+	
+	
+	
+	
+	
+	
+	
 	
 }); //ready function닫기
 
@@ -55,14 +73,15 @@ if($(".category").length>0){
 
 	var category = $(".category").offset().top;
 	var carTab = $(".estimate_online_wrap > .inner > div");
-	var carMenu = $(".category .inbox a");
+
 	
 };
 	
 	
 const cateogryScroll = () =>{
-					
+		
   	var windowTop = $(document).scrollTop();
+	var carMenu = $(".category .inbox a");
 
 	if($(".category").length>0){
 			
@@ -78,24 +97,29 @@ const cateogryScroll = () =>{
 			/* carTab top 위치 저장하기	
 			* 첫 화면에서 첫번째 탭이 무조건 선택되어 있게 하기 위함
 			*/
-			i==0 ? carTabTop[i] = 0 : carTabTop[i] = $(carTab[i]).offset().top-29;
+			i==0 ? carTabTop[i] = 0 : carTabTop[i] = $(carTab[i]).offset().top-100;
 			
 			/*
 			* div의 bottom 위치 저장하기
 			*/
-			carTabBottom[i] = $(carTab[i]).offset().top-29 + carTab[i].offsetHeight;
+			carTabBottom[i] = $(carTab[i]).offset().top-100 + carTab[i].offsetHeight;
 		}
 
 
 		for(var i=0; i<carTabTop.length; i++){
 			
+			
 			/*
 			* car tab에 on 적용
 			*/
 			(windowTop >= carTabTop[i] && windowTop <= carTabBottom[i]) ? $(carMenu[i]).addClass("on") : $(carMenu[i]).removeClass("on");
+			
+			
+			
+			
 		}
 
 	}
-}
+};
 
 
